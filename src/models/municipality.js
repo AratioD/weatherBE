1bd115c12d444b12e18506defe5a9aa75b45a3a9
AratioD <fffb@ddd.com>
2020-02-12 21:35:09 +0200

    first fetch works

--- src/models/municipality.js
+++ src/models/municipality.js
@@ -1,9 +1,38 @@
-var Px = require('px'),
-    fs = require('fs');
+// var Px = require('px'),
+//     fs = require('fs');
 
-const apiUrl = "https://www.stat.fi/ajk/julkistamiskalenteri/juka_json.json"
+//     // a Link to needed api
+// const apiUrl = "http://pxnet2.stat.fi/pxweb/api/v1/fi/StatFin/?query=*&filter="
 
-fs.readFile('apiUrl', 'utf8', function (err, data) {
-    px = new Px(data);
+// fs.readFile('apiUrl', 'utf8', function (err, data) {
+//     px = new Px(data);
+// });
+
+// Create a request variable and assign a new XMLHttpRequest object to it.
+// var request = new XMLHttpRequest()
+
+// // Open a new connection, using the GET request on the URL endpoint
+// request.open('GET', 'http://pxnet2.stat.fi/pxweb/api/v1/fi/StatFin/?query=*&filter=', true)
+
+// request.onload = function() {
+//   // Begin accessing JSON data here
+// }
+
+// // Send request
+// request.send()
+
+const https = require("https");
+const url = "https://pxnet2.stat.fi/PXWeb/api/v1/fi/Kuntien_avainluvut/";
+
+https.get(url, res => {
+  res.setEncoding("utf8");
+  let body = "";
+  res.on("data", data => {
+    body += data;
+  });
+  res.on("end", () => {
+    body = JSON.parse(body);
+    console.log(body);
+  });
 });
 
